dcd8f864a8c68832176000566323867ea8f97fc2
Masudur Rahman <rahmanmasudur0709@gmail.com>
2023-09-09 22:14:08 -0400

    Update: changes with gui

--- GroupProject1/application/gui.py
+++ GroupProject1/application/gui.py
@@ -9,42 +9,42 @@ def open_file():
     file_path = filedialog.askopenfilename(filetypes=[("CSV Files", "*.csv")])
     if file_path:
         csv_file = file_path
-        status_label.config(text=f"File opened: {csv_file}")
+        import_text.config(text=f"File opened: {csv_file}")
 
 def remove_data():
-    def delete_row():
-        order_id = id_entry.get()
+    def delete():
+        row_id = id_data.get()
         data = []
         with open(csv_file, 'r') as file:
             reader = csv.reader(file)
             for row in reader:
-                if row[0] != order_id:
+                if row[0] != row_id:
                     data.append(row)
         with open(csv_file, 'w', newline='') as file:
             writer = csv.writer(file)
             writer.writerows(data)
-        remove_window.destroy()
+        delete_screen.destroy()
 
-    remove_window = tk.Toplevel(gui)
-    remove_window.title("Remove Data")
+    delete_screen = tk.Toplevel(gui)
+    delete_screen.title("Remove Data")
 
-    id_label = tk.Label(remove_window, text="Order ID:")
-    id_label.pack()
-    id_entry = tk.Entry(remove_window)
-    id_entry.pack()
-    delete_button = tk.Button(remove_window, text="Delete", command=delete_row)
+    id_text = tk.Label(delete_screen, text="Enter ID:")
+    id_text.pack()
+    id_data = tk.Entry(delete_screen)
+    id_data.pack()
+    delete_button = tk.Button(delete_screen, text="Delete", command=delete)
     delete_button.pack()
 
 def add_data():
-    def save_data():
+    def save():
         data = [entry.get() for entry in data_entry]
         with open(csv_file, 'a', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(data)
-        add_window.destroy()
+        add_screen.destroy()
 
-    add_window = tk.Toplevel(gui)
-    add_window.title("Add Data")
+    add_screen = tk.Toplevel(gui)
+    add_screen.title("Add Data")
 
     header_data = []
     with open(csv_file, 'r') as file:
@@ -54,42 +54,42 @@ def add_data():
     data_entry = []
 
     for i, header_fields in enumerate(header_data):
-        id_label = tk.Label(add_window, text=header_fields)
-        id_label.grid(row=i, column=0, padx=10, pady=5)
-        id_entry = tk.Entry(add_window)
-        id_entry.grid(row=i, column=1, padx=10, pady=5)
-        data_entry.append(id_entry)
+        id_text = tk.Label(add_screen, text=header_fields)
+        id_text.grid(row=i, column=0, padx=10, pady=5)
+        id_data = tk.Entry(add_screen)
+        id_data.grid(row=i, column=1, padx=10, pady=5)
+        data_entry.append(id_data)
 
-    save_button = tk.Button(add_window, text="Save", command=save_data)
+    save_button = tk.Button(add_screen, text="Save", command=save)
     save_button.grid(row=len(header_data), columnspan=2, padx=10, pady=10)
 
 def query_data():
     def results():
-        id = entry.get()
+        row_data = id_data.get()
         result = []
         with open(csv_file, 'r') as file:
             reader = csv.reader(file)
             for row in reader:
-                if row[0] == id:
+                if row[0] == row_data:
                     result.append(row)
-        result_window = tk.Toplevel(gui)
-        result_window.title("Query Result")
+        result_screen = tk.Toplevel(gui)
+        result_screen.title("Query Result")
         for row in result:
-            tk.Label(result_window, text=", ".join(row)).pack()
+            tk.Label(result_screen, text=", ".join(row)).pack()
 
-    query_window = tk.Toplevel(gui)
-    query_window.title("Query Data")
+    query_screen = tk.Toplevel(gui)
+    query_screen.title("Query Data")
 
-    id_label = tk.Label(query_window, text="Order ID:")
-    id_label.pack()
-    entry = tk.Entry(query_window)
-    entry.pack()
-    query_button = tk.Button(query_window, text="Query", command=results)
+    id_text = tk.Label(query_screen, text="Enter ID:")
+    id_text.pack()
+    id_data = tk.Entry(query_screen)
+    id_data.pack()
+    query_button = tk.Button(query_screen, text="Query", command=results)
     query_button.pack()
 
 def edit_data():
     def edit():
-        id = entry.get()
+        id = id_data.get()
         update = [data_entry.get() for data_entry in fields]
         data = []
         
@@ -102,15 +102,15 @@ def edit_data():
         with open(csv_file, 'w', newline='') as file:
             writer = csv.writer(file)
             writer.writerows(data)
-        edit_window.destroy()
+        edit_screen.destroy()
 
-    edit_window = tk.Toplevel(gui)
-    edit_window.title("Edit Data")
+    edit_screen = tk.Toplevel(gui)
+    edit_screen.title("Edit Data")
 
-    id_label = tk.Label(edit_window, text="Enter ID:")
-    id_label.pack()
-    entry = tk.Entry(edit_window)
-    entry.pack()
+    id_text = tk.Label(edit_screen, text="Enter ID:")
+    id_text.pack()
+    id_data = tk.Entry(edit_screen)
+    id_data.pack()
 
     header_data = []
     with open(csv_file, 'r') as file:
@@ -120,24 +120,25 @@ def edit_data():
     fields = []
 
     for i, header_rows in enumerate(header_data):
-        id_label = tk.Label(edit_window, text=header_rows)
-        id_label.pack()
-        id_entry = tk.Entry(edit_window)
+        id_text = tk.Label(edit_screen, text=header_rows)
+        id_text.pack()
+        id_entry = tk.Entry(edit_screen)
         id_entry.pack()
         fields.append(id_entry)
 
-    edit_button = tk.Button(edit_window, text="Edit", command=edit)
+    edit_button = tk.Button(edit_screen, text="Edit", command=edit)
     edit_button.pack()
 
 gui = tk.Tk()
-gui.title("demo")
+gui.title("CSV Editor")
+gui.geometry("800x400")
 
 import_button = tk.Button(gui, text="Import", command=open_file)
 remove_button = tk.Button(gui, text="Remove", command=remove_data)
 add_button = tk.Button(gui, text="Add", command=add_data)
 query_button = tk.Button(gui, text="Query", command=query_data)
 edit_button = tk.Button(gui, text="Edit", command=edit_data)
-status_label = tk.Label(gui, text="No file opened")
+import_text = tk.Label(gui, text="No file opened")
 
 
 import_button.pack()
@@ -145,6 +146,6 @@ remove_button.pack()
 add_button.pack()
 query_button.pack()
 edit_button.pack()
-status_label.pack()
+import_text.pack()
 
 gui.mainloop()
