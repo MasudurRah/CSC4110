6cf0db5c45ec6296ce551b5fac60b506a9aadf9f
Masudur Rahman <rahmanmasudur0709@gmail.com>
2023-10-06 19:14:58 -0400

    WIP

--- GroupProject2/application/wordle.py
+++ GroupProject2/application/wordle.py
@@ -1,28 +1,45 @@
 import tkinter as tk
 import turtle
 import random
-import re  # Import the re module for regular expressions
+import re
+import time
 
 # Load words from the "GroupProject2/files/sgb-words.txt" file into a list
 with open("GroupProject2/files/sgb-words.txt", "r") as file:
     word_list = file.read().splitlines()
 
 # Initialize the game state
-current_word = None  # Initialize with None to indicate the game hasn't started
+current_word = None
 attempts_left = 5
 current_attempt = 0
 previous_attempts = []
+timer_seconds = 60
+
+# Function to update the timer label
+def update_timer():
+    global timer_seconds
+    timer_label.config(text=f"Time left: {timer_seconds} seconds")
+    if timer_seconds > 0:
+        timer_seconds -= 1
+        root.after(1000, update_timer)
+    else:
+        results_label.config(text="Time's up! You lost. The word was: " + current_word)
+        play_again_button.config(state=tk.NORMAL)
+        entry.config(state=tk.DISABLED)
 
 # Function to enable the input box after a delay
 def enable_input():
     entry.config(state=tk.NORMAL)
 
+# Function to initialize the game
 def initialize_game():
-    global current_word, attempts_left, current_attempt, previous_attempts
+    global current_word, attempts_left, current_attempt, previous_attempts, timer_seconds
     current_word = random.choice(word_list)
     attempts_left = 5
     current_attempt = 0
     previous_attempts = []
+    timer_seconds = 60
+    update_timer()
     results_label.config(text="")
     entry.config(state=tk.NORMAL)
     entry.delete(0, tk.END)
@@ -50,6 +67,10 @@ entry.pack()
 results_label = tk.Label(root, text="")
 results_label.pack()
 
+# Create a Label for displaying the timer
+timer_label = tk.Label(root, text=f"Time left: {timer_seconds} seconds")
+timer_label.pack()
+
 # Create a "Play Again" button
 play_again_button = tk.Button(root, text="Play Again", command=initialize_game, state=tk.DISABLED)
 play_again_button.pack()
@@ -67,7 +88,7 @@ def draw_attempt(user_input, feedback, y_offset):
         elif feedback[i] == "Y":
             t.fillcolor("yellow")
         else:
-            t.fillcolor("red")  # Box turns red if the letter isn't within the word
+            t.fillcolor("red")
 
         t.begin_fill()
         for _ in range(4):
@@ -76,7 +97,7 @@ def draw_attempt(user_input, feedback, y_offset):
         t.end_fill()
 
         t.penup()
-        t.goto(-80 + i * 40, 105 - y_offset)  # Adjusted the y-coordinate to move the letters up within the box
+        t.goto(-80 + i * 40, 105 - y_offset)
         t.pendown()
         t.pencolor("black")
         t.write(user_input[i], align="center", font=("Arial", 16, "normal"))
@@ -90,7 +111,7 @@ def check_word():
     user_input = entry.get().lower()
     entry.delete(0, tk.END)
 
-    # Check if the input consists of only English letters
+
     if not re.match("^[a-zA-Z]*$", user_input):
         results_label.config(text="Enter a 5-letter word using only English letters")
         return
@@ -101,7 +122,7 @@ def check_word():
 
     if current_word is None:
         current_word = random.choice(word_list)
-        play_again_button.config(state=tk.NORMAL)  # Enable "Play Again" button
+        play_again_button.config(state=tk.NORMAL)
 
     current_attempt += 1
     feedback = ""
@@ -115,26 +136,26 @@ def check_word():
 
     results_label.config(text=feedback)
 
-    # Save the current attempt and draw it
     previous_attempts.append((user_input, feedback))
 
-    # Disable the input box before starting to draw
     entry.config(state=tk.DISABLED)
 
     draw_attempt(user_input, feedback, (current_attempt - 1) * 40)
 
     if feedback == "GGGGG":
         results_label.config(text="Congratulations! You guessed the word: " + current_word)
-        play_again_button.config(state=tk.NORMAL)  # Enable "Play Again" button
+        play_again_button.config(state=tk.NORMAL)
     elif current_attempt >= 5:
         results_label.config(text="You lost. The word was: " + current_word)
-        play_again_button.config(state=tk.NORMAL)  # Enable "Play Again" button
+        play_again_button.config(state=tk.NORMAL)
 
-    # Enable the input box after 5 seconds
     root.after(400, enable_input)
 
 # Bind the Enter key to the check_word function
 entry.bind("<Return>", lambda event=None: check_word())
+
+# Initialize the game when the program starts
+initialize_game()
 
 # Start the tkinter main loop
 root.mainloop()
