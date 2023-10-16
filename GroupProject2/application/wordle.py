7999759990cbb2c1dc62495125f63680d6e3480a
hamam <hd6336@wayne.edu>
2023-10-16 18:41:51 -0400

    Final wordle changes

--- GroupProject2/application/wordle.py
+++ GroupProject2/application/wordle.py
@@ -1,56 +1,82 @@
+"""Modules to import tkinter, turtle, random, and re functionality"""
 import tkinter as tk
 import turtle
 import random
 import re
-import time
 
 # Load words from the "GroupProject2/files/sgb-words.txt" file into a list
-with open("GroupProject2/files/sgb-words.txt", "r") as file:
+with open("GroupProject2/files/sgb-words.txt", "r", encoding="utf-8") as file:
     word_list = file.read().splitlines()
 
 # Initialize the game state
-current_word = None
-attempts_left = 5
-current_attempt = 0
-previous_attempts = []
-timer_seconds = 90  # Initialize the timer to 60 seconds
-timer_active = False  # Indicates whether the timer is active
-game_won = False  # Flag to track if the game is won
+CURRENT_WORD = None
+ATTEMPTS_LEFT = 5
+CURRENT_ATTEMPT = 0
+PREVIOUS_ATTEMPTS = []
+TIMER_SECONDS = 90
+TIMER_ACTIVE = False
+GAME_WON = False
+
+# Create a function to clear the turtle graphics
+def clear_screen():
+    """Clear screen function"""
+    t.reset()
+    t.speed(0)
+    t.penup()
+    t.goto(0, 0)
+    t.pendown()
 
 # Function to update the timer label
 def update_timer():
-    global timer_seconds, timer_active
-    timer_label.config(text=f"Time left: {timer_seconds} seconds")
-    if timer_seconds > 0 and timer_active:
-        timer_seconds -= 1
+    """Update timer function"""
+    global TIMER_SECONDS, TIMER_ACTIVE
+    timer_label.config(text=f"Time left: {TIMER_SECONDS} seconds")
+    if TIMER_SECONDS > 0 and TIMER_ACTIVE:
+        TIMER_SECONDS -= 1
         root.after(1000, update_timer)
     else:
-        if current_attempt < 5 and not game_won:
-            results_label.config(text="Time's up! You lost. The word was: " + current_word)
-        play_again_button.config(state=tk.NORMAL)  # Enable "Play Again" button
+        if CURRENT_ATTEMPT < 5 and not GAME_WON:
+            results_label.config(text="Time's up! You lost. The word was: " + CURRENT_WORD)
+        play_again_button.config(state=tk.NORMAL)
         entry.config(state=tk.DISABLED)
-        timer_active = False  # Stop the timer
+        TIMER_ACTIVE = False
 
 # Function to enable the input box after a delay
 def enable_input():
+    """Function to enable the input"""
     entry.config(state=tk.NORMAL)
+
+# Function to initialize the app
+def initialize_app():
+    """Function to initialize app on startup"""
+    canvas.config(bg="green")
+    clear_screen()
+    t.color("yellow")
+    t.write("Welcome to Wordle", align="center", font=("Arial", 24, "bold"))
+    entry.delete(0, tk.END)
+    entry.insert(0, "Enter a word to start")
+    entry.config(state=tk.DISABLED)
+    play_again_button.config(text="Start Game", state=tk.NORMAL)
 
 # Function to initialize the game
 def initialize_game():
-    global current_word, attempts_left, current_attempt, previous_attempts, timer_seconds, timer_active, game_won
-    current_word = None
-    attempts_left = 5
-    current_attempt = 0
-    previous_attempts = []
-    timer_seconds = 90  # Reset the timer to 60 seconds
-    timer_active = False  # Set timer_active to False initially
-    game_won = False  # Reset game_won flag
-    timer_label.config(text=f"Time left: {timer_seconds} seconds")
+    """Function to initialize game on startup"""
+    global CURRENT_WORD, ATTEMPTS_LEFT, CURRENT_ATTEMPT
+    global PREVIOUS_ATTEMPTS, TIMER_SECONDS, TIMER_ACTIVE, GAME_WON
+    CURRENT_WORD = None
+    ATTEMPTS_LEFT = 5
+    CURRENT_ATTEMPT = 0
+    PREVIOUS_ATTEMPTS = []
+    TIMER_SECONDS = 90
+    TIMER_ACTIVE = False
+    GAME_WON = False
+    timer_label.config(text=f"Time left: {TIMER_SECONDS} seconds")
     results_label.config(text="")
     entry.config(state=tk.NORMAL)
     entry.delete(0, tk.END)
-    canvas.delete("all")  # Clear the canvas
-    play_again_button.config(state=tk.DISABLED)  # Disable the "Play Again" button until the game ends
+    canvas.config(bg="white")
+    play_again_button.config(text="Play Again", state=tk.DISABLED)
+    clear_screen()
 
 # Create a tkinter window
 root = tk.Tk()
@@ -74,15 +100,12 @@ results_label = tk.Label(root, text="")
 results_label.pack()
 
 # Create a Label for displaying the timer
-timer_label = tk.Label(root, text=f"Time left: {timer_seconds} seconds")
+timer_label = tk.Label(root, text=f"Time left: {TIMER_SECONDS} seconds")
 timer_label.pack()
-
-# Create a "Play Again" button
-play_again_button = tk.Button(root, text="Play Again", command=initialize_game, state=tk.DISABLED)
-play_again_button.pack()
 
 # Function to draw a single attempt
 def draw_attempt(user_input, feedback, y_offset):
+    """Function to draw the user attempts"""
     for i in range(5):
         t.penup()
         t.goto(-100 + i * 40, 100 - y_offset)
@@ -110,10 +133,11 @@ def draw_attempt(user_input, feedback, y_offset):
 
 # Create a function to check the user's input and update the visual representation
 def check_word():
-    global current_word, current_attempt, attempts_left, previous_attempts, timer_active, game_won
+    """Function to check and validate user input"""
+    global CURRENT_WORD, CURRENT_ATTEMPT, TIMER_ACTIVE, GAME_WON
 
-    if current_attempt >= 5 or game_won:
-        return  # No more input after 5 attempts or if the game is won
+    if CURRENT_ATTEMPT >= 5 or GAME_WON:
+        return
 
     user_input = entry.get().lower()
     entry.delete(0, tk.END)
@@ -126,48 +150,52 @@ def check_word():
         results_label.config(text="Enter a 5-letter word")
         return
 
-    if current_word is None:
-        current_word = random.choice(word_list)
-        timer_active = True  # Start the timer when the user inputs the first word
-        update_timer()  # Start the timer immediately
+    if CURRENT_WORD is None:
+        CURRENT_WORD = random.choice(word_list)
+        TIMER_ACTIVE = True
+        update_timer()
 
-    current_attempt += 1
+    CURRENT_ATTEMPT += 1
     feedback = ""
     for i in range(5):
-        if user_input[i] == current_word[i]:
+        if user_input[i] == CURRENT_WORD[i]:
             feedback += "G"
-        elif user_input[i] in current_word:
+        elif user_input[i] in CURRENT_WORD:
             feedback += "Y"
         else:
             feedback += "X"
 
     results_label.config(text=feedback)
 
-    previous_attempts.append((user_input, feedback))
+    PREVIOUS_ATTEMPTS.append((user_input, feedback))
 
     entry.config(state=tk.DISABLED)
 
-    draw_attempt(user_input, feedback, (current_attempt - 1) * 40)
+    draw_attempt(user_input, feedback, (CURRENT_ATTEMPT - 1) * 40)
 
     if feedback == "GGGGG":
-        results_label.config(text="Congratulations! You guessed the word: " + current_word)
-        game_won = True
-        play_again_button.config(state=tk.NORMAL)
-        entry.config(state=tk.DISABLED)  # Disable input box when the game is won
-        timer_active = False  # Stop the timer when the game is won
-    elif current_attempt >= 5:
-        results_label.config(text="You lost. The word was: " + current_word)
+        results_label.config(text="Congratulations! You guessed the word: " + CURRENT_WORD)
+        GAME_WON = True
         play_again_button.config(state=tk.NORMAL)
         entry.config(state=tk.DISABLED)
-        timer_active = False  # Stop the timer when the game is lost
+        TIMER_ACTIVE = False
+    elif CURRENT_ATTEMPT >= 5:
+        results_label.config(text="You lost. The word was: " + CURRENT_WORD)
+        play_again_button.config(state=tk.NORMAL)
+        entry.config(state=tk.DISABLED)
+        TIMER_ACTIVE = False
 
     root.after(400, enable_input)
 
 # Bind the Enter key to the check_word function
 entry.bind("<Return>", lambda event=None: check_word())
 
-# Initialize the game when the program starts
-initialize_game()
+# Create a "Play Again" button
+play_again_button = tk.Button(root, text="Start Game", command=initialize_game, state=tk.DISABLED)
+play_again_button.pack()
+
+# Initialize the app when the program starts
+initialize_app()
 
 # Start the tkinter main loop
 root.mainloop()
